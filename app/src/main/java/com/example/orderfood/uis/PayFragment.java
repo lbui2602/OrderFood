cc6ad4a7c18be6934e41e443fdba5992f5c9cf1a
lbui2602 <buiducluong2602@gmail.com>
2023-11-16 10:08:48 +0700

    lan4

--- app/src/main/java/com/example/orderfood/uis/PayFragment.java
+++ app/src/main/java/com/example/orderfood/uis/PayFragment.java
@@ -12,10 +12,10 @@ import com.example.orderfood.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DrinkFragment#newInstance} factory method to
+ * Use the {@link PayFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DrinkFragment extends Fragment {
+public class PayFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class DrinkFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DrinkFragment() {
+    public PayFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class DrinkFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DrinkFragment.
+     * @return A new instance of fragment PayFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DrinkFragment newInstance(String param1, String param2) {
-        DrinkFragment fragment = new DrinkFragment();
+    public static PayFragment newInstance(String param1, String param2) {
+        PayFragment fragment = new PayFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class DrinkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drink, container, false);
+        return inflater.inflate(R.layout.fragment_pay, container, false);
     }
 }
--- app/src/main/java/com/example/orderfood/uis/UserFragment.java
+++ app/src/main/java/com/example/orderfood/uis/UserFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.orderfood.R;
+import com.example.orderfood.models.database.DBHelper;
 
 /**
  * A simple {@link Fragment} subclass.
